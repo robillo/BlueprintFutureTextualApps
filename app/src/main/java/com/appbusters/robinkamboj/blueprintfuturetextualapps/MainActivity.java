5fa18e826ec9e1537b80943c92987808ce07567f
Robin Kamboj <dev.robinkamboj@gmail.com>
2016-12-26 10:18:25 +0530

    trying to set action bar icon

--- app/src/main/java/com/appbusters/robinkamboj/blueprintfuturetextualapps/MainActivity.java
+++ app/src/main/java/com/appbusters/robinkamboj/blueprintfuturetextualapps/MainActivity.java
@@ -3,6 +3,7 @@ package com.appbusters.robinkamboj.blueprintfuturetextualapps;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -17,6 +18,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        android.app.ActionBar actionBar= this.getActionBar();
+        if (actionBar != null) {
+            actionBar.setIcon(R.mipmap.ic_launcher);
+        }
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
